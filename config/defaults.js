44a3c6acfa0d7e91e5b82d414d6eb43a6dfc1942
codedream <codedreamfy@outlook.com>
2017-10-12 00:37:32 +0800

    链接数据库

--- config/defaults.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  port: 3000,
-  session: {
-    secret: 'nodedemo',
-    key: 'nodedemo',
-    maxAge: 2592000000
-  },
-  mongodb: 'mongodb://loclahost:27017/nodedemo'
-}
--- index.js
+++ index.js
@@ -3,12 +3,11 @@ var express = require('express');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var flash = require('connect-flash');
-var config = require('config-lite');
+var config = require('config-lite')(__dirname);
 var routes = require('./routes');
 var pkg = require('./package');
 
 var app = express();
-
 // 设置模板目录
 app.set('views', path.join(__dirname, 'views'));
 // 设置模板引擎为 ejs
@@ -20,6 +19,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   name: config.session.key,// 设置 cookie 中保存 session id 的字段名称
   secret: config.session.secret,// 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+  resave: true,// 强制更新 session
+  saveUninitialized: false,// 设置为 false，强制创建一个 session，即使用户未登录
   cookie: {
     maxAge: config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
   },
@@ -27,8 +28,22 @@ app.use(session({
     url: config.mongodb// mongodb 地址
   })
 }));
-// flash 中间价，用来显示通知
+// flash 中间件，用来显示通知
 app.use(flash());
+
+// 设置模板全局常量
+app.locals.blog = {
+  title: pkg.name,
+  description: pkg.description
+};
+
+// 添加模板必需的三个变量
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user;
+  res.locals.success = req.flash('success').toString();
+  res.locals.error = req.flash('error').toString();
+  next();
+});
 
 // 路由
 routes(app);
